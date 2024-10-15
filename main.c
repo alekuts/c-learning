fd982a6e59eb91f21e2e0e0cd3717dfa5e3222ca
alekuts <alexanderkutsovol@gmail.com>
2024-10-16 00:16:16 +0300

    update

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main(){
-  printf("Hello, \m world \n");
+float i = 1;
 
+int main(){
+  while(i < 100000){
+    i = i * 2;
+    printf("%8.1f\n", i);
+  }
   return 0;
 }
