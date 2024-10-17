b1509207802b3bc9ec7759c67acc12d433e8e828
alekuts <alexanderkutsovol@gmail.com>
2024-10-17 21:11:49 +0300

    update

--- main.c
+++ main.c
@@ -1,18 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    char *gig = (char *)malloc(1073741824 * sizeof(char)); // 1 ГіБ
+int main(int argc, char **argv)
+{
+  if(argc != 4)
+  {
+    printf("Invalid count of argument\n");
+    return 1;
+  }
 
-    if (gig == NULL) {
-        printf("Помилка виділення пам'яті!\n");
-        return 1;
-    }
-for (size_t i = 0; i < 1073741824; i++) {
-        gig[i] = 0;
-    }
+  float n1 = atof(argv[1]);
+  char n2 = argv[2][0];
+  float n3 = atof(argv[3]);
 
-    fgets(gig, sizeof(gig), stdin);
 
-    return 0;
+  float res = 0;
+  switch(n2)
+  {
+    case '+':
+      res = n1 + n3;
+      break;
+    case '-':
+      res = n1 - n3;
+      break;
+    case '*':
+      res = n1 * n3;
+      break;
+    case '/':
+      res = n1 / n3;
+      break;
+    default:
+      printf("Invalid expression\n");
+      return 1;
+  }
+
+  printf("Result: %.1f\n", res);
+
+  return 0;
 }
