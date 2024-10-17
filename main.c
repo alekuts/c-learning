b0af74f04c4f411efe5bb742a1d89c33f853fbaa
alekuts <alexanderkutsovol@gmail.com>
2024-10-17 18:08:06 +0300

    update

--- main.c
+++ main.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-float i = 1;
+int main() {
+    char *gig = (char *)malloc(1073741824 * sizeof(char)); // 1 ГіБ
 
-int main(){
-  while(i < 100000){
-    i = i * 2;
-    printf("%8.1f\n", i);
-  }
-  return 0;
+    if (gig == NULL) {
+        printf("Помилка виділення пам'яті!\n");
+        return 1;
+    }
+for (size_t i = 0; i < 1073741824; i++) {
+        gig[i] = 0;
+    }
+
+    fgets(gig, sizeof(gig), stdin);
+
+    return 0;
 }
